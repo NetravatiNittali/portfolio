0cc27af27e06f75569d953c3e7cf5c4ba22f8720
NetravatiNittali <62070205+NetravatiNittali@users.noreply.github.com>
2023-01-16 11:22:23 +0530

    Add files via upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/73713152-2258-4a8c-9639-3e9e875fe359" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 '>
                 <p className='text-4xl  font-bold inline border-b-4 border-[#FFFF33] text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - netravati.snittali @gmail.com</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or send me an email - netravati.snittali @gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5.5" placeholder='Message'></textarea>
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' autocomplete="off" required />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='"demo@gmail.com' name='email' autocomplete="off" required/>
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5.5" placeholder='Message' autocomplete="off"  min ="5" required></textarea>
             <button className='text-white border-2 hover:bg-[#FFFF33] hover:border-[#FFFF33] hover:text-black hover:font-bold px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
